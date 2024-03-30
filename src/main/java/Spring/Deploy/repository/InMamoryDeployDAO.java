8c392cbdec0be89e1976785094bcfca088404c68
miloe <miloegrich@gmail.com>
2024-03-30 14:02:51 +0300

    add xml load from file

--- src/main/java/Spring/Deploy/repository/InMamoryDeployDAO.java
+++ src/main/java/Spring/Deploy/repository/InMamoryDeployDAO.java
@@ -3,11 +3,9 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -15,9 +13,7 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import Spring.Deploy.model.Deploy;
 import org.springframework.stereotype.Repository;
-import org.xml.sax.SAXException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -58,7 +54,7 @@ public class InMamoryDeployDAO {
     }
 
     public List<Deploy> loadDeploysFromXmlFile() {
-        File file = new File("DeployFile.xml");
+        File file = new File("DeploysFile.xml");
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         Document doc = null;
         try {
@@ -71,8 +67,8 @@ public class InMamoryDeployDAO {
         NodeList rootChilds = rootNode.getChildNodes();
         String maindep = null;
         Node DeployNode = null;
-        for(int i = 0; i < rootChilds.getLength(); i++){
-            if (rootChilds.item(i).getNodeType() != Node.ELEMENT_NODE){
+        for (int i = 0; i < rootChilds.getLength(); i++) {
+            if (rootChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {
                 continue;
             }
             if (rootChilds.item(i).getNodeName().equals("root")) {
@@ -82,35 +78,35 @@ public class InMamoryDeployDAO {
                 DeployNode = rootChilds.item(i);
             }
         }
-        if (DeployNode == null){
+        if (DeployNode == null) {
             return null;
         }
         List<Deploy> deployList = new ArrayList<>();
         NodeList DeployChilds = DeployNode.getChildNodes();
-        for(int i = 0; i < DeployChilds.getLength(); i++){
-            if (DeployChilds.item(i).getNodeType() != Node.ELEMENT_NODE){
+        for (int i = 0; i < DeployChilds.getLength(); i++) {
+            if (DeployChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {
                 continue;
             }
-            if(DeployChilds.item(i).getNodeName().equals("Deploy")){
+            if (DeployChilds.item(i).getNodeName().equals("Deploy")) {
                 continue;
             }
-             String Tracking_number = "";
-             String Adress_Off_Sender = "";
-             String Adress_Off_Appointment = "";
-             String Status = "";
-             String Date_Of_Depature = "";
+            String Tracking_number = "";
+            String Adress_Off_Sender = "";
+            String Adress_Off_Appointment = "";
+            String Status = "";
+            String Date_Of_Depature = "";
             NodeList deployChilds = DeployChilds.item(i).getChildNodes();
-            for (int j = 0; j < deployChilds.getLength(); j ++){
-                if (deployChilds.item(i).getNodeType() != Node.ELEMENT_NODE){
+            for (int j = 0; j < deployChilds.getLength(); j++) {
+                if (deployChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {
                     continue;
                 }
-                switch (deployChilds.item(j).getNodeName()){
+                switch (deployChilds.item(j).getNodeName()) {
                     case "Tracking_number": {
                         Tracking_number = deployChilds.item(j).getTextContent();
 
                     }
                     case "Adress_Off_Sender": {
-                        Adress_Off_Sender  = deployChilds.item(j).getTextContent();
+                        Adress_Off_Sender = deployChilds.item(j).getTextContent();
 
                     }
                     case "Adress_Off_Appointment": {
@@ -127,97 +123,58 @@ public class InMamoryDeployDAO {
                     }
                 }
             }
-            Deploy deploy = new Deploy(Tracking_number,Adress_Off_Sender,Adress_Off_Appointment,Status,Date_Of_Depature);
+            Deploy deploy = new Deploy(Tracking_number, Adress_Off_Sender, Adress_Off_Appointment, Status, Date_Of_Depature);
             deployList.add(deploy);
         }
         return deployList;
     }
 
+    public void saveDeploysToXmlFile(Deploy deploys) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.newDocument();
 
+            Element root = doc.createElement("Deploys");
+            doc.appendChild(root);
+
+            for (Deploy deploy : deploys) {
+                Element deployElement = doc.createElement("Deploy");
+                root.appendChild(deployElement);
+
+                Element trackingNumber = doc.createElement("Tracking_number");
+                trackingNumber.appendChild(doc.createTextNode(deploy.getTracking_number()));
+                deployElement.appendChild(trackingNumber);
+
+                Element addressOffSender = doc.createElement("Adress_Off_Sender");
+                addressOffSender.appendChild(doc.createTextNode(deploy.getAdress_Off_Sender()));
+                deployElement.appendChild(addressOffSender);
+
+                Element addressOffAppoint = doc.createElement("Adress_Off_Appoint");
+                addressOffAppoint.appendChild(doc.createTextNode(deploy.getAdress_Off_Appointment()));
+                deployElement.appendChild(addressOffAppoint);
+
+                Element Status = doc.createElement("Status");
+                Status.appendChild(doc.createTextNode(deploy.getStatus()));
+                deployElement.appendChild(Status);
+
+                Element dateOfDepature = doc.createElement("Date_Of_Depature");
+                dateOfDepature.appendChild(doc.createTextNode(deploy.getDate_Of_Depature()));
+                deployElement.appendChild(dateOfDepature);
+
+
+            }
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("DeploysFile.xml"));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
-//    public void saveDeploysToXmlFile(List<Deploy> deploys) {
-//        try {
-//            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//            DocumentBuilder builder = factory.newDocumentBuilder();
-//            Document doc = builder.newDocument();
-//
-//            Element root = doc.createElement("Deploys");
-//            doc.appendChild(root);
-//
-//            for (Deploy deploy : deploys) {
-//                Element deployElement = doc.createElement("Deploy");
-//                root.appendChild(deployElement);
-//
-//                Element trackingNumber = doc.createElement("Tracking_number");
-//                trackingNumber.appendChild(doc.createTextNode(deploy.getTracking_number()));
-//                deployElement.appendChild(trackingNumber);
-//
-//                Element addressOffSender = doc.createElement("Adress_Off_Sender");
-//                addressOffSender.appendChild(doc.createTextNode(deploy.getAdress_Off_Sender()));
-//                deployElement.appendChild(addressOffSender);
-//
-//                Element addressOffAppoint = doc.createElement("Adress_Off_Appoint");
-//                addressOffAppoint.appendChild(doc.createTextNode(deploy.getAdress_Off_Appointment()));
-//                deployElement.appendChild(addressOffAppoint);
-//
-//                Element Status = doc.createElement("Status");
-//                Status.appendChild(doc.createTextNode(deploy.getStatus()));
-//                deployElement.appendChild(Status);
-//
-//                Element dateOfDepature = doc.createElement("Date_Of_Depature");
-//                dateOfDepature.appendChild(doc.createTextNode(deploy.getDate_Of_Depature()));
-//                deployElement.appendChild(dateOfDepature);
-//
-//
-//            }
-//            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//            Transformer transformer = transformerFactory.newTransformer();
-//            DOMSource source = new DOMSource(doc);
-//            StreamResult result = new StreamResult(new File("DeploysFile.xml"));
-//            transformer.transform(source, result);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public List<Deploy> loadDeploysFromXmlFile() {
-//        List<Deploy> deploys = new ArrayList<>();
-//
-//        try {
-//            File file = new File("DeploysFile.xml");
-//            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//            DocumentBuilder builder = factory.newDocumentBuilder();
-//            Document doc = builder.parse(file);
-//
-//            Element root = doc.getDocumentElement();
-//            NodeList nodeList = root.getElementsByTagName("Deploy");
-//
-//            for (int i = 0; i < nodeList.getLength(); i++) {
-//                Node node = nodeList.item(i);
-//                if (node.getNodeType() == Node.ELEMENT_NODE) {
-//                    Element element = (Element) node;
-//
-//                    String tracking_number = element.getElementsByTagName("Tracking_number").item(0).getTextContent();
-//                    String address_off_sender = element.getElementsByTagName("Adress_Off_Sender").item(0).getTextContent();
-//                    String address_off_appoint = element.getElementsByTagName("Adress_Off_Appoint").item(0).getTextContent();
-//                    String status = element.getElementsByTagName("Status").item(0).getTextContent();
-//                    String date_off_depature = element.getElementsByTagName("Date_Of_Depature").item(0).getTextContent();
-//
-//
-//                    Deploy deploy = new Deploy(tracking_number, address_off_sender,address_off_appoint,status,date_off_depature);
-//                    deploys.add(deploy);
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return deploys;
-//    }
-
 
 
 
